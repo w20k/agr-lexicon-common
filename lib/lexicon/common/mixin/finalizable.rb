98116815715ec31fcc1398530d2b7e97d80e9911
Rémi Piotaix <remi@piotaix.fr>
2021-02-18 13:47:16 +0100

    Fix bad location of the do_call helper

--- lib/lexicon/common/mixin/finalizable.rb
+++ lib/lexicon/common/mixin/finalizable.rb
@@ -13,30 +13,30 @@ module Lexicon
 
               e
             end
+
+            # Empty Array and Hash splats are handled correctly starting ruby 2.7:
+            # if both args and kwargs are empty, no parameters are sent.
+            if ::Semantic::Version.new(RUBY_VERSION).satisfies?('>= 2.7.0')
+              private def do_call(obj, method, args, kwargs)
+                obj.send(method, *args, **kwargs)
+              end
+            else
+              private def do_call(obj, method, args, kwargs)
+                if args.empty? && kwargs.empty?
+                  obj.send(method)
+                elsif args.empty?
+                  obj.send(method, **kwargs)
+                elsif kwargs.empty?
+                  obj.send(method, *args)
+                else
+                  obj.send(method, *args, **kwargs)
+                end
+              end
+            end
           end
         end
 
         private
-
-          # Empty Array and Hash splats are handled correctly starting ruby 2.7:
-          # if both args and kwargs are empty, no parameters are sent.
-          if ::Semantic::Version.new(RUBY_VERSION).satisfies?('>= 2.7.0')
-            def do_call(obj, method, args, kwargs)
-              obj.send(method, *args, **kwargs)
-            end
-          else
-            def do_call(obj, method, args, kwargs)
-              if args.empty? && kwargs.empty?
-                obj.send(method)
-              elsif args.empty?
-                obj.send(method, **kwargs)
-              elsif kwargs.empty?
-                obj.send(method, *args)
-              else
-                obj.send(method, *args, **kwargs)
-              end
-            end
-          end
 
           def finalize
             raise StandardError.new("Finalizer is not implemented in #{self.class.name}")
